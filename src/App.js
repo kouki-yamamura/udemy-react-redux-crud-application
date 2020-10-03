507672c10eed48788fac49d083ae47732e1e00ec
kouki <kouki.1992.0218@gmail.com>
2020-10-03 23:16:47 +0900

    learn props of component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import React from 'react';
 
 
-// function App() {
-//   const log = ()=>{console.log("I am click.")};
-//   const dom = <input type="button" value="ボタン" onClick={log} />
-//   return (
-//     <React.Fragment>
-//       <label htmlFor="bar">bar</label>
-//       <div>{dom}</div>
-//     </React.Fragment>
-//     )
-// }
+//コンポーネントの作り方
+//関数コンポーネント
+
+//propsの渡し方
+//main:1 sub:2 for create
 
 const App = ()=>{
-  return <div>Hi!</div>
+  const profiles = [
+    {name:"taro",age:10},
+    {name:"hanako",age:21},
+  ]
+
+  return(
+    <div>
+      {
+        profiles.map((profiles,index)=>{
+          return <User name={profiles.name} age={profiles.age} key={index} />
+        })
+      }
+    </div>
+  )
 }
 
-const Cat = ()=>{
-  return <div>にゃーお</div>
+const User = (props) => {
+  return <div>Hi , I am {props.name}, and {props.age} years old!</div>
 }
+
 
 export default App;
