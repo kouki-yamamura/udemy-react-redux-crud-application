4285ca71883ad4fc583a4d0f40bb699008fd76d4
kouki <kouki.1992.0218@gmail.com>
2020-10-03 22:03:26 +0900

    createclass components and functional component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React from 'react';
 
 
-function App() {
-  const log = ()=>{console.log("I am click.")};
-  const dom = <input type="button" value="ボタン" onClick={log} />
-  return (
-    <React.Fragment>
-      <label htmlFor="bar">bar</label>
-      <div>{dom}</div>
-    </React.Fragment>
-    
+// function App() {
+//   const log = ()=>{console.log("I am click.")};
+//   const dom = <input type="button" value="ボタン" onClick={log} />
+//   return (
+//     <React.Fragment>
+//       <label htmlFor="bar">bar</label>
+//       <div>{dom}</div>
+//     </React.Fragment>
+//     )
+// }
 
+const App = ()=>{
+  return <div>Hi!</div>
+}
 
-    
-    
-    )
+const Cat = ()=>{
+  return <div>にゃーお</div>
 }
 
 export default App;
