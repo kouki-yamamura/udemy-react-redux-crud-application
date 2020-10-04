efdb3838e498806948c1afeb0101ff720e18e2c2
kouki <kouki.1992.0218@gmail.com>
2020-10-04 16:07:05 +0900

    implement actioin and action creator

---
+++ src/actions/index.js
@@ -0,0 +1,13 @@
+export const INCREMENT = 'INCREMENT'
+export const DECREMENT = 'DECREMENT'
+
+
+const INCREMENT = ''
+
+export const incriment = () => ({
+    type:INCREMENT    
+})
+
+export const decrement = () => ({
+    type:DECREMENT
+})
